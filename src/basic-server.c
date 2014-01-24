f172f549ad658644e65e852868332c127d66fc89
adilbaig <adil.baig@aidezigns.com>
2014-01-25 02:25:10 +0530

    basic-server

---
+++ src/basic-server.c
@@ -0,0 +1,43 @@
+#include "../lib/common.h"
+#include "../lib/network.h"
+
+void process_request(int);
+
+int main(int argc, char **argv) {
+
+    char *prog_name;
+    prog_name = argv[0];
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <listening port> \n", prog_name);
+        return EXIT_FAILURE;
+    }
+
+    int port = atoi(argv[1]);
+    int listen_fd = Open_listenfd(port);
+    printf("%s listening INTENTLY on %d \n", prog_name, port);
+
+    struct sockaddr addr;
+    socklen_t addr_len = sizeof(addr);
+    int accept_fd;
+    while (1) {
+        accept_fd = Accept(listen_fd, &addr, &addr_len);
+        printf("Received! \n");
+
+        process_request(accept_fd);
+        close(accept_fd);
+    }
+
+    Close(listen_fd);
+    printf("%s shutting down gracefully \n", prog_name);
+
+    return EXIT_SUCCESS;
+}
+
+void process_request(int fd) {
+
+    struct http_request_data data = http_parse_request(fd);
+    http_print_request_data(&data);
+
+    write(fd, "received your message\n", 25);
+}
